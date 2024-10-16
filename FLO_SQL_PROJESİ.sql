55881585dbf11b66d77d9dfbdfc181a880787446
Nurdan <160058995+2nur@users.noreply.github.com>
2024-10-16 14:57:18 +0300

    Update FLO_SQL_PROJESİ.sql

--- FLO_SQL_PROJESİ.sql
+++ FLO_SQL_PROJESİ.sql
@@ -1,65 +1,65 @@
 
---SORU 1 Customers isimli bir veritabanıve verilen veri setindeki değişkenleri içerecekFLO isimli bir tablo oluşturma:
+--SORU 1 Customers isimli bir veritabanÄ±nda verilen veri setindeki deÄŸiÅŸkenleri iÃ§erecek FLO isimli bir tablo oluÅŸturma:
 CREATE DATABASE CUSTOMERS
 
 CREATE TABLE FLO (
-	master_id							VARCHAR(50),
+	master_id						VARCHAR(50),
 	order_channel						VARCHAR(50),
 	last_order_channel					VARCHAR(50),
 	first_order_date					DATE,
 	last_order_date						DATE,
-	last_order_date_online				DATE,
-	last_order_date_offline				DATE,
-	order_num_total_ever_online			INT,
-	order_num_total_ever_offline		INT,
-	customer_value_total_ever_offline	FLOAT,
-	customer_value_total_ever_online	FLOAT,
-	interested_in_categories_12			VARCHAR(50),
-	store_type							VARCHAR(10)
+	last_order_date_online				        DATE,
+	last_order_date_offline					DATE,
+	order_num_total_ever_online				INT,
+	order_num_total_ever_offline				INT,
+	customer_value_total_ever_offline			FLOAT,
+	customer_value_total_ever_online			FLOAT,
+	interested_in_categories_12				VARCHAR(50),
+	store_type						VARCHAR(10)
 );
 
 
 
---SORU 2: Kaç farklı müşterinin alışveriş yaptığını gösterecek sorguyu yazma.
+--SORU 2: KaÃ§ farklÄ± mÃ¼ÅŸterinin alÄ±ÅŸveriÅŸ yaptÄ±ÄŸÄ±nÄ± gÃ¶sterecek sorguyu yazma.
 SELECT COUNT(DISTINCT(master_id)) AS DISTINCT_KISI_SAYISI FROM FLO;
 
 
---SORU 3:Online sipariş sayısı 500'Den fazla olan müşterileri ve alışveriş yaparken kullandıkları kanalları bul
+--SORU 3:Online sipariÅŸ sayÄ±sÄ± 500'Den fazla olan mÃ¼ÅŸterileri ve alÄ±ÅŸveriÅŸ yaparken kullandÄ±klarÄ± kanallarÄ± bulma.
 SELECT 
 master_id,order_channel
 FROM FLO
 WHERE order_num_total_ever_online >500
 
 
---SORU 4: Toplam yapılan alışveriş sayısı ve ciroyu getirecek sorguyu yazma.
+--SORU 4: Toplam yapÄ±lan alÄ±ÅŸveriÅŸ sayÄ±sÄ± ve ciroyu getirecek sorguyu yazma.
 SELECT 
 	SUM(order_num_total_ever_offline + order_num_total_ever_online) AS TOPLAM_SIPARIS_SAYISI,
 	ROUND(SUM(customer_value_total_ever_offline + customer_value_total_ever_online), 2) AS TOPLAM_CIRO
 FROM FLO
 
 
---SORU 5:  Alışveriş başına ortalama ciroyu getirecek sorguyu yazma. 
+--SORU 5:  AlÄ±ÅŸveriÅŸ baÅŸÄ±na ortalama ciroyu getirecek sorguyu yazma. 
 SELECT 
-ROUND((sum(customer_value_total_ever_online+customer_value_total_ever_offline) / SUM(order_num_total_ever_online + order_num_total_ever_offline)),2) AS ORTCİRO
+ROUND((sum(customer_value_total_ever_online+customer_value_total_ever_offline) / SUM(order_num_total_ever_online + order_num_total_ever_offline)),2) AS ORTCIRO
 FROM FLO
 
---SORU 6: En son alışveriş yapılan kanal (last_order_channel) üzerinden yapılan alışverişlerin toplam ciro ve alışveriş sayılarını getirme
+--SORU 6: En son alÄ±ÅŸveriÅŸ yapÄ±lan kanal (last_order_channel) Ã¼zerinden yapÄ±lan alÄ±ÅŸveriÅŸlerin toplam ciro ve alÄ±ÅŸveriÅŸ sayÄ±larÄ±nÄ± getirme
 select 
 last_order_channel AS KANAL,
-SUM(customer_value_total_ever_online+customer_value_total_ever_offline) AS TOPLAM_CİRO,
-SUM(order_num_total_ever_online+order_num_total_ever_offline) AS ALIŞVERİŞ_SAYISI_TOPLAM
+SUM(customer_value_total_ever_online+customer_value_total_ever_offline) AS TOPLAM_CIRO,
+SUM(order_num_total_ever_online+order_num_total_ever_offline) AS ALIÅVERÄ°Å_SAYISI_TOPLAM
 from FLO
 GROUP BY last_order_channel
 
 
---SORU 7: Store type kırılımında elde edilen toplam ciroyu getiren sorgu
+--SORU 7: Store type kÄ±rÄ±lÄ±mÄ±nda elde edilen toplam ciroyu getiren sorgu
 SELECT store_type MAGAZATURU, 
        ROUND(SUM(customer_value_total_ever_offline + customer_value_total_ever_online), 2)  AS TOPLAM_CIRO 
 FROM FLO 
 GROUP BY store_type
  
 
---SORU 8: Yıl kırılımında alışveriş sayılarını getirecek sorguyu yaz (Yıl olarak müşterinin ilk alışveriş tarihi (first_order_date) yılını baz alma)
+--SORU 8: YÄ±l kÄ±rÄ±lÄ±mÄ±nda alÄ±ÅŸveriÅŸ sayÄ±larÄ±nÄ± getirecek sorguyu yaz (YÄ±l olarak mÃ¼ÅŸterinin ilk alÄ±ÅŸveriÅŸ tarihi (first_order_date) yÄ±lÄ±nÄ± baz alma)
 SELECT 
 YEAR(first_order_date)  as YIL, 
 SUM(order_num_total_ever_offline + order_num_total_ever_online)  AS SIPARIS_SAYISI
@@ -67,7 +67,7 @@ FROM  FLO
 GROUP BY YEAR(first_order_date)
 
 
---SORU 9: En son alışveriş yapılan kanal kırılımında alışveriş başına ortalama ciroyu hesaplayacak sorgu
+--SORU 9: En son alÄ±ÅŸveriÅŸ yapÄ±lan kanal kÄ±rÄ±lÄ±mÄ±nda alÄ±ÅŸveriÅŸ baÅŸÄ±na ortalama ciroyu hesaplayacak sorgu
 SELECT 
 last_order_channel, 
  ROUND(SUM(customer_value_total_ever_offline + customer_value_total_ever_online),2)  AS TOPLAM_CIRO,
@@ -77,42 +77,42 @@ FROM FLO
 GROUP BY last_order_channel
 
 
---SORU 10: Son 12 ayda en çok ilgi gören kategoriyi getiren sorguyu yazma
+--SORU 10: Son 12 ayda en Ã§ok ilgi gÃ¶ren kategoriyi getiren sorguyu yazma
 SELECT interested_in_categories_12, COUNT(*) FREKANS_BILGISI 
 FROM FLO
 GROUP BY interested_in_categories_12
 ORDER BY interested_in_categories_12 ASC
 
 
---SORU 11:  En çok tercih edilen store_type bilgisini getiren sorguyu yazma
+--SORU 11:  En Ã§ok tercih edilen store_type bilgisini getiren sorguyu yazma
 SELECT TOP 1   
 store_type, COUNT(*) FREKANS_BILGISI 
 FROM FLO 
 GROUP BY store_type 
-ORDER BY store_type ASC   --ORDER BY 2 DESC ŞEKLİNDEDE YAZILABİLİR
+ORDER BY store_type ASC   --ORDER BY 2 DESC ÅEKLÄ°NDEDE YAZILABÄ°LÄ°R
 
 
 
---SORU 12: En son alışveriş yapılan kanal (last_order_channel) bazında, en çok ilgi gören kategoriyi ve bu kategoriden ne kadarlık alışveriş yapıldığını getiren sorguyu yazma.
+--SORU 12: En son alÄ±ÅŸveriÅŸ yapÄ±lan kanal (last_order_channel) bazÄ±nda, en Ã§ok ilgi gÃ¶ren kategoriyi ve bu kategoriden ne kadarlÄ±k alÄ±ÅŸveriÅŸ yapÄ±ldÄ±ÄŸÄ±nÄ± getiren sorguyu yazma.
 SELECT DISTINCT last_order_channel,
 (
 	SELECT top 1 interested_in_categories_12
 	FROM FLO  WHERE last_order_channel=f.last_order_channel
 	group by interested_in_categories_12
 	order by SUM(order_num_total_ever_online+order_num_total_ever_offline) desc 
-) AS EN_ÇOK_İLGİ_GÖREN_KATEGORİ,
+) AS EN_Ã‡OK_ILGI_GÃ–REN_KATEGORI,
 (
 	SELECT top 1 SUM(order_num_total_ever_online+order_num_total_ever_offline)
 	FROM FLO  WHERE last_order_channel=f.last_order_channel
 	group by interested_in_categories_12
 	order by SUM(order_num_total_ever_online+order_num_total_ever_offline) desc 
-) AS ALIŞVERİŞ_SAYISI
+) AS ALIÅVERÄ°Å_SAYISI
 FROM FLO F
 
 
 
 
---SORU 13: En çok alışverişyapan kişinin ID’sini getiren sorgu 
+--SORU 13: En Ã§ok alÄ±ÅŸveriÅŸ yapan kiÅŸinin IDâ€™sini getiren sorgu 
  SELECT TOP 1 master_id   		    
 	FROM FLO 
 	GROUP BY master_id 
@@ -120,7 +120,7 @@ ORDER BY  SUM(customer_value_total_ever_offline + customer_value_total_ever_onli
 
 
 
---SORU 14: En çok alışveriş yapan kişinin alışveriş başına ortalama cirosunu ve alışveriş yapma gün ortalamasını (alışveriş sıklığını) getiren sorgu
+--SORU 14: En Ã§ok alÄ±ÅŸveriÅŸ yapan kiÅŸinin alÄ±ÅŸveriÅŸ baÅŸÄ±na ortalama cirosunu ve alÄ±ÅŸveriÅŸ yapma gÃ¼n ortalamasÄ±nÄ± (alÄ±ÅŸveriÅŸ sÄ±klÄ±ÄŸÄ±nÄ±) getiren sorgu
 SELECT D.master_id,
 ROUND((D.TOPLAM_CIRO / D.TOPLAM_SIPARIS_SAYISI),2) SIPARIS_BASINA_ORTALAMA,
 ROUND((DATEDIFF(DAY, first_order_date, last_order_date)/D.TOPLAM_SIPARIS_SAYISI ),1) AS ALISVERIS_GUN_ORT
@@ -135,13 +135,13 @@ ORDER BY TOPLAM_CIRO DESC
 ) D
 
 
---SORU 15: En çok alışverişyapan (ciro bazında) ilk 100 kişinin alışveriş yapma gün ortalamasını (alışverişsıklığını) getiren sorgu
+--SORU 15: En Ã§ok alÄ±ÅŸveriÅŸ yapan (ciro bazÄ±nda) ilk 100 kiÅŸinin alÄ±ÅŸveriÅŸ yapma gÃ¼n ortalamasÄ±nÄ± (alÄ±ÅŸveriÅŸ sÄ±klÄ±ÄŸÄ±nÄ±) getiren sorgu
 SELECT  
 D.master_id,
        D.TOPLAM_CIRO,
 	   D.TOPLAM_SIPARIS_SAYISI,
        ROUND((D.TOPLAM_CIRO / D.TOPLAM_SIPARIS_SAYISI),2) SIPARIS_BASINA_ORTALAMA,
-	   DATEDIFF(DAY, first_order_date, last_order_date) ILK_SN_ALVRS_GUN_FRK,
+	   DATEDIFF(DAY, first_order_date, last_order_date) ILK_GÃœN_ALIÅVERÄ°Å_GUN_FARK,
 	  ROUND((DATEDIFF(DAY, first_order_date, last_order_date)/D.TOPLAM_SIPARIS_SAYISI ),1) ALISVERIS_GUN_ORT	 
   FROM
 (
@@ -154,7 +154,7 @@ ORDER BY TOPLAM_CIRO DESC
 ) D
 
 
---SORU 16: En son alışverişyapılan kanal (last_order_channel) kırılımındaen çok alışverişyapan müşteriyi getiren sorguyu yazma
+--SORU 16: En son alÄ±ÅŸveriÅŸ yapÄ±lan kanal (last_order_channel) kÄ±rÄ±lÄ±mÄ±ndan Ã§ok alÄ±ÅŸveriÅŸ yapan mÃ¼ÅŸteriyi getiren sorguyu yazma
 SELECT DISTINCT last_order_channel,
 (
 	SELECT top 1 master_id
@@ -173,44 +173,44 @@ SELECT DISTINCT last_order_channel,
 FROM FLO F
 
 
---SORU 17:  En son alışverişyapan kişinin ID’ sini getiren sorguyu yazma. (Max son tarihte birden fazla alışveriş yapan ID bulunmakta.) 
+--SORU 17:  En son alÄ±ÅŸveriÅŸ yapan kiÅŸinin IDâ€™ sini getiren sorguyu yazma. (Max son tarihte birden fazla alÄ±ÅŸveriÅŸ yapan ID bulunmakta.) 
 SELECT master_id,last_order_date FROM FLO
 WHERE last_order_date=(SELECT MAX(last_order_date) FROM FLO)
 
 
---SORU 18:Belirli bir tarihten sonra sipariş veren müşteriler('2019-08-21' / '2020-06-13')
+--SORU 18:Belirli bir tarihten sonra sipariÅŸ veren mÃ¼ÅŸteriler('2019-08-21' / '2020-06-13')
 
 SELECT master_id, last_order_date
 FROM FLO
 WHERE last_order_date_online between '2019-08-21' and '2020-06-13' 
 
 
---SORU 19: Sadece online ya da sadece offline alışveriş yapan müşteriler
+--SORU 19: Sadece online ya da sadece offline alÄ±ÅŸveriÅŸ yapan mÃ¼ÅŸteriler
 SELECT master_id
 FROM FLO
 WHERE order_num_total_ever_online = 0 OR order_num_total_ever_offline = 0
 
---SORU 20 :Mağaza türüne göre müşteri dağılımı
+--SORU 20 :MaÄŸaza tÃ¼rÃ¼ne gÃ¶re mÃ¼ÅŸteri daÄŸÄ±lÄ±mÄ±
 SELECT store_type, COUNT(*) AS customer_count
 FROM FLO
 GROUP BY store_type
 ORDER BY customer_count DESC
 
 
---SORU 21:İlgilendiği kategorilere göre müşteri sayısı
+--SORU 21:Ä°lgilendiÄŸi kategorilere gÃ¶re mÃ¼ÅŸteri sayÄ±sÄ±
 SELECT interested_in_categories_12, COUNT(master_id) AS customer_count
 FROM FLO
 GROUP BY interested_in_categories_12
 ORDER BY customer_count DESC;
 
 
---SORU 22 :2021 yılında sipariş veren müşterileri bulma
+--SORU 22 :2021 yÄ±lÄ±nda sipariÅŸ veren mÃ¼ÅŸterileri bulma
 SELECT master_id, last_order_date
 FROM FLO
 WHERE YEAR(last_order_date) = 2021
 
 
---SORU 23:Toplam online ve offline sipariş sayısı aynı olan müşterileri listeme
+--SORU 23:Toplam online ve offline sipariÅŸ sayÄ±sÄ± aynÄ± olan mÃ¼ÅŸterileri listeme
 SELECT master_id, order_num_total_ever_online, order_num_total_ever_offline
 FROM FLO
 WHERE order_num_total_ever_online = order_num_total_ever_offline;
